6c8beba3eed6ddfa01a5cd50bd6f1fd1b9751796
Adamo Tonete <adamobr@gmail.com>
2018-10-30 19:00:51 -0200

    Fixed

--- start_replicaset1_auth_keyfile.sh
+++ start_replicaset1_auth_keyfile.sh
@@ -16,7 +16,7 @@ cp -R ~/percona/single_instance/* ~/percona/rs1_1/
 chmod +x ~/percona/PL18/config_files/prepare.sh
 ~/percona/PL18/config_files/prepare.sh
 mkdir ~/percona/PL18/config_files/replaced
-mv ~/percona/PL18/config_files/*.cfg ~/percona/PL18/config_files/replaced/
+mv ~/percona/PL18/config_files/replicaset/*.cfg ~/percona/PL18/config_files/replaced/
 
 ~/percona/mongo/mongod -f ~/percona/PL18/config_files/replaced/rs11.cfg
 ~/percona/mongo/mongod -f ~/percona/PL18/config_files/replaced/rs12.cfg
--- start_shard_auth_keyfile.sh
+++ start_shard_auth_keyfile.sh
@@ -6,8 +6,20 @@ sleep 5
 
 
 chmod +x ~/percona/PL18/config_files/prepare.sh
+
+#cleaning all
+rm /home/adamo/percona/PL18/config_files/replaced -rf
+cd ~/percona/PL18/config_files/shard
+./clear.sh
+
+cd ~/percona/PL18/config_files/replicaset/
+./clear.sh
+cd ~
+
 ~/percona/PL18/config_files/prepare.sh
 mkdir ~/percona/PL18/config_files/replaced
+
+#configs should be ok here
 mv ~/percona/PL18/config_files/shard/*.cfg ~/percona/PL18/config_files/replaced/
 
 ~/percona/mongo/mongod -f ~/percona/PL18/config_files/replaced/rs11.cfg
@@ -16,5 +28,10 @@ mv ~/percona/PL18/config_files/shard/*.cfg ~/percona/PL18/config_files/replaced/
 ~/percona/mongo/mongod -f ~/percona/PL18/config_files/replaced/rs21.cfg
 ~/percona/mongo/mongod -f ~/percona/PL18/config_files/replaced/rs22.cfg
 ~/percona/mongo/mongod -f ~/percona/PL18/config_files/replaced/rs23.cfg
+~/percona/mongo/mongod -f ~/percona/PL18/config_files/replaced/srv_cfg.cfg
 
-~/percona/mongo/mongo --port 27013
+~/percona/mongo/mongo --port 27019 --eval "rs.initiate()"
+
+sleep(10)
+~/percona/mongo/mongos -f ~/percona/PL18/config_files/replaced/mongos.cfg
+
